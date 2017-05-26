d28243c7318180a0867f454ca89e0bcfa03bb356
Strider <strange.transistor@gmail.com>
2017-05-27 00:59:36 +0300

    test.less.

--- test/test.case.js
+++ test/test.case.js
@@ -12,7 +12,7 @@ var compare = require('dir-compare').compareSync
 
 var less = (...args) => require('less').render(...args)
 
-var filename = rootpath('axiom/index.less')
+var filename = rootpath('test/test.less')
 var contents = read(filename)
 
 
