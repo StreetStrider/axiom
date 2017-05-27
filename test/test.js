a31258e22fddb328ced0686aa7af4f7b9d04de3c
Strider <strange.transistor@gmail.com>
2017-05-27 03:09:14 +0300

    test: +glob.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
 
+var wait_for_all = (...args) => Promise.all(...args)
+
 var tmpdir = require('os').tmpdir
 var randomstring = require('randomstring').generate
 
 var rootpath = require('rootpath')(__dirname, '..')
 var rootpath_tmp = require('rootpath')(tmpdir(), 'axiom', randomstring(8))
+
+var find = require('globule').find
+var base = require('path').basename
 
 var read  = require('fs-sync').read
 var write = require('fs-sync').write
@@ -12,18 +17,37 @@ var compare = require('dir-compare').compareSync
 
 var less = (...args) => require('less').render(...args)
 
-var filename = rootpath('test/case/index.less')
-var contents = read(filename)
 
-
+// 
 console.log(rootpath_tmp())
 
-less(contents, { filename })
-.then(it => it.css)
-.then(result =>
+Promise.resolve()
+.then(() =>
 {
-	write(rootpath_tmp('index.css'), result)
+	return find(rootpath('test/case/*.less'))
+})
+.then(paths =>
+{
+	return paths.map(path => base(path, '.less'))
+})
+.then(names =>
+{
+	return names.map(name =>
+	{
+		var filename = rootpath('test/case/', name + '.less')
+		var contents = read(filename)
 
+		return less(contents, { filename })
+		.then(it => it.css)
+		.then(result =>
+		{
+			write(rootpath_tmp(name + '.css'), result)
+		})
+	})
+})
+.then(wait_for_all)
+.then(it =>
+{
 	var tmp = rootpath_tmp()
 	var fix = rootpath('fixture')
 
